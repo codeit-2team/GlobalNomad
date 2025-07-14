fb1512e0b2c65e3ce9be0bd9e6ae90464f89683e
DESKTOP-JBD7NHD\User <qhrud0130@gmail.com>
2025-07-14 19:29:49 +0900

    feat: footer 디테일 수정

--- globalnomad/src/components/Footer/Footer.tsx
+++ globalnomad/src/components/Footer/Footer.tsx
@@ -1,29 +1,46 @@
 // components/Footer.tsx
 
-import Image from 'next/image';
+import IconFacebook from '@/app/assets/icons/facebook';
+import IconInstagram from '@/app/assets/icons/instagram';
+import IconYouTube from '@/app/assets/icons/youtube';
+import IconTwitter from '@/app/assets/icons/twitter';
 
 export default function Footer() {
   return (
-    <footer className="w-full bg-[#0C1B0D] text-white px-4 py-8">
-      <div className="max-w-screen-xl mx-auto flex flex-col md:flex-row items-center justify-between gap-4 text-sm text-gray-300">
-        
-        {/* 좌측: 저작권 */}
-        <div className="text-center md:text-left">
-          ©codeit - 2023
+    <footer className="w-full bg-nomad text-[#676767] pt-32 pb-66 md:px-111 md:pb-111 text-lg">
+      <div className="max-w-1200 mx-auto flex flex-col md:flex-row items-center justify-between gap-4">
+        {/* mobile 버전 */}
+        <div className="md:hidden flex flex-row gap-12">
+          {/* 좌측: 저작권 */}
+          <div className="text-center md:text-left">
+            ©codeit - 2023
+          </div>
+
+          {/* 중앙: 내비게이션 */}
+          <div className="flex justify-center gap-30">
+            <a href="#" className="hover:text-white transition-colors">Privacy Policy</a>
+            <a href="#" className="hover:text-white transition-colors">FAQ</a>
+          </div>
         </div>
 
-        {/* 중앙: 내비게이션 */}
-        <div className="flex gap-4 justify-center">
-          <a href="#" className="hover:text-white transition-colors">Privacy Policy</a>
-          <a href="#" className="hover:text-white transition-colors">FAQ</a>
-        </div>
+        {/* pc,tablet 버전 */}
+          {/* 좌측: 저작권 */}
+          <div className="hidden md:block text-center md:text-left">
+            ©codeit - 2023
+          </div>
+
+          {/* 중앙: 내비게이션 */}
+          <div className="hidden md:flex justify-center gap-30">
+            <a href="#" className="hover:text-white transition-colors">Privacy Policy</a>
+            <a href="#" className="hover:text-white transition-colors">FAQ</a>
+          </div>
 
         {/* 우측: SNS 아이콘 */}
-        <div className="flex gap-3 justify-center">
-          <a href="#"><Image src="/img/icon/facebook.svg" alt="facebook" width={18} height={18} /></a>
-          <a href="#"><Image src="/img/icon/twitter.svg" alt="twitter" width={18} height={18} /></a>
-          <a href="#"><Image src="/img/icon/youtube.svg" alt="youtube" width={18} height={18} /></a>
-          <a href="#"><Image src="/img/icon/instagram.svg" alt="instagram" width={18} height={18} /></a>
+        <div className="flex gap-12 justify-center mt-24 md:mt-0">
+          <a href="#"><IconFacebook /></a>
+          <a href="#"><IconTwitter /></a>
+          <a href="#"><IconYouTube /></a>
+          <a href="#"><IconInstagram /></a>
         </div>
       </div>
     </footer>
