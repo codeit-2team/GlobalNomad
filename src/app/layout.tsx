289bc8d08bea05a502764303815f3be4b970c499
DESKTOP-JBD7NHD\User <qhrud0130@gmail.com>
2025-07-18 16:43:52 +0900

    chore: layout 파일 내 헤더푸터 경로 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,12 @@
-'use client';
-
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import './globals.css';
-import Pagination from '@/components/Pagination';
-import { useState } from 'react';
-import { se } from 'date-fns/locale';
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const [page, setPage] = useState(2);
-
   return (
     <html lang='ko'>
       <body className='bg-white text-gray-900'>
@@ -21,10 +14,7 @@ export default function RootLayout({
         <Header />
 
         {/* 메인 콘텐츠 */}
-        <main className='min-h-screen pt-70'>
-          {children}
-          <Pagination currentPage={1} totalPage={10} onPageChange={setPage} />
-        </main>
+        <main className='min-h-screen pt-70'>{children}</main>
 
         {/* 공통 푸터 */}
         <Footer />
