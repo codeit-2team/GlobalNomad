a84bc803fac7946c402814eb6e7dc539d4e2eaf8
박광민 <rhkdals0242@naver.com>
2025-07-24 22:38:10 +0900

    fix: 카카오 로그인, 회원가입 페이지 CSR 강제 적용을 위한 dynamic 설정 추가

--- src/app/(non-header)/oauth/kakao/sign-in/page.tsx
+++ src/app/(non-header)/oauth/kakao/sign-in/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+export const dynamic = 'force-dynamic';
+
 import useUserStore from '@/stores/authStore';
 import axios from 'axios';
 import { useRouter, useSearchParams } from 'next/navigation';
