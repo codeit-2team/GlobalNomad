acd1d6891cb740fe93636d4d6646f9117878bf57
DESKTOP-JBD7NHD\User <qhrud0130@gmail.com>
2025-07-28 15:40:41 +0900

    fix: 빌드 에러 수정

--- src/app/(with-header)/page.tsx
+++ src/app/(with-header)/page.tsx
@@ -1,12 +1,14 @@
 'use client';
 
 import { useState } from 'react';
+
 import BannerSection from '@/app/(with-header)/components/BannerSection';
 import PopularExperiences from '@/app/(with-header)/components/PopularExperiences';
+
 import ExperienceList from './components/ExperienceList';
 
 export default function HomePage() {
-  const [searchKeyword, setSearchKeyword] = useState('');
+  const [_searchKeyword, setSearchKeyword] = useState('');
 
   return (
     <main>
