a93fead0fdb1b207eba00e09bc043dd1c458df57
박광민 <rhkdals0242@naver.com>
2025-07-29 11:35:47 +0900

    fix: 이미지 중복 선언으로 빌드 오류 수정 완료

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,11 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   // Docker 배포를 위한 standalone 모드 활성화
   // 해당 설정은 프로덕션 빌드 시 필요한 파일만 .next/standalone 폴더에 복사됨.
-  images: {
-    domains: ['sprint-fe-project.s3.ap-northeast-2.amazonaws.com'],
-  },
   output: 'standalone',
 
   // 외부 이미지 도메인 허용
   images: {
+    domains: ['sprint-fe-project.s3.ap-northeast-2.amazonaws.com'],
     remotePatterns: [
       {
         protocol: 'https',
