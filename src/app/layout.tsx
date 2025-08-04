8c75c142a8c482a87cfd66dcc4210d54dc276c26
박광민 <rhkdals0242@naver.com>
2025-08-04 23:29:20 +0900

    feat: SEO 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,28 @@ import { Toaster } from 'sonner';
 export const metadata = {
   title: 'GlobalNomad',
   description: '글로벌 노마드 체험 플랫폼',
+  keywords: ['글로벌 노마드', '체험 플랫폼', '여행', '노마드', '다양한 활동'],
+  authors: [{ name: 'GlobalNomad', url: 'https://globalnomad.site/' }],
+  openGraph: {
+    title: 'GlobalNomad',
+    description: '전 세계 체험을 한 눈에!',
+    url: 'https://globalnomad.site/',
+    siteName: 'GlobalNomad',
+    locale: 'ko_KR',
+    type: 'website',
+    images: [
+      {
+        url: 'https://globalnomad.site/og-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'GlobalNomad 메인 배너 이미지',
+      },
+    ],
+    icons: {
+      icon: '/favicon.ico',
+      apple: '/apple-touch-icon.png',
+    },
+  },
 };
 
 export default function RootLayout({
